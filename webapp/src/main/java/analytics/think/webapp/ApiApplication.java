530fc840ef14478e36aba6110db53553adf979ef
Munish Sharma <munish.sharma@coda.global>
2020-02-08 20:23:46 +0530

    initial commit
    spring boilerplate code

---
+++ webapp/src/main/java/analytics/think/webapp/ApiApplication.java
@@ -0,0 +1,13 @@
+package analytics.think.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ webapp/src/test/java/analytics/think/webapp/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package analytics.think.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
